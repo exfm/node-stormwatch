883a14894e59a66bc8ee83ebca7e984e7e2838c8
Drew Harris <contac@grmnygrmny.com>
2012-10-26 16:59:08 -0400

    Attempting to pull down cloud metrics

--- node-stormwatch.js
+++ node-stormwatch.js
@@ -1,3 +1,28 @@
 "use strict";
 
-// app goes here
+var aws = require('plata'),
+	sequence = require('sequence'),
+	when = require('when'),
+	request = require('superagent'),
+	getConfig = require('junto'),
+	config;
+
+getConfig('development').then(function(c){
+	config = c;
+	aws.connect(config.aws);
+});
+
+sequence().then(function(next){
+	aws.onConnected(next);
+}).then(function(next){
+	var startTime = new Date(new Date().getTime() - 1200000),
+		endTime = new Date();
+	aws.cloudWatch.getMetricStatistics('AWS/ELB', 'Latency', 60, startTime.toISOString(), endTime.toISOString(), {
+		'Average': '1'}, 'Seconds', {
+		'LoadBalancerName': 'production'
+	}).then(function(data){
+		var resp = data;
+		console.log(resp);
+		// console.log(data);
+	});
+});
